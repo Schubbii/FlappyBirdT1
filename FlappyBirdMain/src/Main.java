278649fe5305d0360fb49dc505ce62c9140a64ed
KiraG2005 <kgrauf@gmx.de>
2025-12-05 12:03:15 +0100

    Add basic JFrame setup for Flappy Bird window
    Replaces placeholder code with initialization of a non-resizable JFrame titled 'Flappy Bird', centered on the screen with specified dimensions. Sets default close operation to exit the application.

--- FlappyBirdMain/src/Main.java
+++ FlappyBirdMain/src/Main.java
@@ -1,17 +1,25 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import javax.swing.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Spielfenster
+
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);  //in der Mitte des Screens plaziert
+        frame.setResizable(false);   // Feld kann nicht größenverändert werden
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // wenn x gedrückt, Programm beendet
+
         }
     }
-}
 
-//test test
+
